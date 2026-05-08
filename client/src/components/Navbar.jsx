1cfc4ed4ccdd25e168d47d9b79e9fa7f4c190a7f
CHRISTYPUNITHA <154300792+prawinkumar2k@users.noreply.github.com>
2026-05-08 14:46:36 +0530

    design: replace MongoDB logo and favicon with premium custom TalentFlow SVG assets

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ export default function Navbar() {
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
           <img 
-            alt="MongoDB logo" 
-            className="h-10 inline" 
-            src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"
+            alt="TalentFlow logo" 
+            className="h-12 inline" 
+            src="/logo.svg"
           />
         </NavLink>
 
